08cbe423acc9548749a611db6dce0ea6fa77cf95
Motechit <otikomosunmola@gmail.com>
2023-11-02 19:15:50 -0700

    advanced tasks

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,123 @@
+#include "binary_trees.h"
+
+levelorder_queue_t *create_node(binary_tree_t *node);
+void free_queue(levelorder_queue_t *head);
+void pint_push(binary_tree_t *node, levelorder_queue_t *head,
+		levelorder_queue_t **tail, void (*func)(int));
+void pop(levelorder_queue_t **head);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+
+/**
+ * create_node - It creates a new levelorder_queue_t node
+ * @node: This is the binary tree node for the new node to contain
+ * Return: If an error occurs - NULL, otherwise, a ptr to the new node
+ */
+
+levelorder_queue_t *create_node(binary_tree_t *node)
+{
+	levelorder_queue_t *new;
+
+	new = malloc(sizeof(levelorder_queue_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->node = node;
+	new->next = NULL;
+
+	return (new);
+}
+
+/**
+ * free_queue - It frees a levelorder_queue_t queue
+ * @head: This is the ptr to the head of the queue
+ */
+
+void free_queue(levelorder_queue_t *head)
+{
+	levelorder_queue_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
+
+/**
+ * pint_push - It runs a fxn on a given binary tree node nd pushes its children
+ * into a levelorder_queue_t queue
+ * @node: This is the binary tree node to print and push
+ * @head: This is the double ptr to the head of the queue
+ * @tail: This is the double pr to the tail of the queue
+ * @func: This is the ptr to the fxn to call on @node
+ * Description: It exits with a status code of 1 at malloc failure
+ */
+
+void pint_push(binary_tree_t *node, levelorder_queue_t *head,
+		levelorder_queue_t **tail, void (*func)(int))
+{
+	levelorder_queue_t *new;
+
+	func(node->n);
+	if (node->left != NULL)
+	{
+		new = create_node(node->left);
+		if (new == NULL)
+		{
+			free_queue(head);
+			exit(1);
+		}
+		(*tail)->next = new;
+		*tail = new;
+	}
+	if (node->right != NULL)
+	{
+		new = create_node(node->right);
+		if (new == NULL)
+		{
+			free_queue(head);
+			exit(1);
+		}
+		(*tail)->next = new;
+		*tail = new;
+	}
+}
+
+/**
+ * pop - Fxn pops the head of a levelorder_queue_t queue
+ * @head: This is the double ptr to the head of the queue
+ */
+
+void pop(levelorder_queue_t **head)
+{
+	levelorder_queue_t *tmp;
+
+	tmp = (*head)->next;
+	free(*head);
+	*head = tmp;
+}
+
+/**
+ * binary_tree_levelorder - It traverse binary tree using level-order traversal
+ * @tree: This is the ptr to the root node of the tree
+ * @func: This is the ptr to the fxn to call for each node
+ */
+
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	levelorder_queue_t *head, *tail;
+
+	if (tree == NULL || func == NULL)
+		return;
+
+	head = tail = create_node((binary_tree_t *)tree);
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		pint_push(head->node, head, &tail, func);
+		pop(&head);
+	}
+}
---
+++ 104-binary_tree_rotate_right.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_right - It Right-rotates a binary tree
+ * @tree: This is the ptr to the root node
+ * Return: This is the ptr to the new root node
+ */
+
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
+{
+	binary_tree_t *pivot, *tmp;
+
+	if (tree == NULL || tree->left == NULL)
+		return (NULL);
+
+	pivot = tree->left;
+	tmp = pivot->right;
+	pivot->right = tree;
+	tree->left = tmp;
+	if (tmp != NULL)
+		tmp->parent = tree;
+	tmp = tree->parent;
+	tree->parent = pivot;
+	pivot->parent = tmp;
+	if (tmp != NULL)
+	{
+		if (tmp->left == tree)
+			tmp->left = pivot;
+		else
+			tmp->right = pivot;
+	}
+
+	return (pivot);
+}
